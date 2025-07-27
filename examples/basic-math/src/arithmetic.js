4b20e5421eae7d53d51b12f3a9a1e672732e87a0
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2025-07-27 01:57:43 +0100

    fix: enhance add function to handle numeric strings and validate inputs

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,10 +1,21 @@
 export const add = (a, b) => {
-    return a + b;
+  if (typeof a === 'string') {
+    a = Number(a);
+  }
+  if (typeof b === 'string') {
+    b = Number(b);
+  }
+
+  if (isNaN(a) || isNaN(b)){
+    throw new Error("Invalid input: both arguments must be numbers or numeric strings.");
+  }
+  
+  return a + b;
 };
 
 export const subtract = (a, b) => {
-    return a - b;
+  return a - b;
 };
 
 export const multiply = (a, b) => a * b;
-export const divide = (a, b) => a/b;
+export const divide = (a, b) => a / b;
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -5,6 +5,16 @@ describe('add', () => {
   test('add two number', () => {
     expect(add(5, 5)).toBe(10);
   });
+
+  it('should covert number to string and add', () => {
+    expect(add('10', '10')).toBe(20);
+  });
+
+  it('should throw an error if inputs are not numbers or numeric strings', () => {
+    expect(() => add('a', 5)).toThrow(
+      'Invalid input: both arguments must be numbers or numeric strings.',
+    );
+  });
 });
 
 describe('subtract', () => {

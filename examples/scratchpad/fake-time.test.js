04c8e626606e4d53e97d8f52f0b574805ec3bece
chukwuekeuzoma <chukwuekeuzoma@gmail.com>
2025-09-23 15:13:34 +0100

    test: refactor delay function tests to include setup and teardown for fake timers

--- examples/scratchpad/fake-time.test.js
+++ examples/scratchpad/fake-time.test.js
@@ -1,6 +1,4 @@
-import { vi, describe, it, expect } from 'vitest';
-
-vi.useFakeTimers();
+import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest';
 
 function delay(callback) {
   setTimeout(() => {
@@ -9,12 +7,26 @@ function delay(callback) {
 }
 
 describe('delay function', () => {
-  it.todo('should call callback after delay', () => {});
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date(2020, 1, 1));
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it('should call callback after delay', () => {
+    const callback = vi.fn();
+    delay(callback);
+    vi.advanceTimersToNextTimer();
+    expect(callback).toHaveBeenCalled();
+  });
 });
 
 // this part was done by me
 describe('mathsFunctions', () => {
-  it('adding two numbers', ()=>{
+  it('adding two numbers', () => {
     const sum = (a, b) => a + b;
     expect(sum(7, 3)).toBe(10);
   });
@@ -22,5 +34,5 @@ describe('mathsFunctions', () => {
   it('subtracting of two numbers', () => {
     const subtract = (a, b) => a - b;
     expect(subtract(7, 3)).toBe(4);
-  })
+  });
 });
